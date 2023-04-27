f8117c6d162f8d408e4b804b244f4aad1ad17113
An70nAnderss0n <113804609+An70nAnderss0n@users.noreply.github.com>
2023-04-27 13:47:01 +0200

    Adds js-logic for each square

--- script.js
+++ script.js
@@ -3,13 +3,32 @@
 const App = {
   // All of our selected HTML elements
   $: {
-    menu: document.querySelector('.menu'),
-    menuItems: document.querySelector('.items'),
+    menu: document.querySelector('[data-id="menu"]'),
+    menuItems: document.querySelector('[data-id="menu-items"]'),
+    resetBtn: document.querySelector('[data-id="reset-btn"]'),
+    newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+    squares: document.querySelectorAll('[data-id="square"]')
   },
 
   init() {
+
+    console.log(App.$.squares);
+
     App.$.menu.addEventListener('click', (event) => {
       App.$.menuItems.classList.toggle('hidden');
+    });
+
+    App.$.resetBtn.addEventListener('click', event => {
+      console.log('Reset the game');
+    })
+
+    App.$.newRoundBtn.addEventListener('click', event => {
+      console.log('Add a new round');
+    })
+    App.$.squares.forEach(square => {
+      square.addEventListener('click', event => {
+        console.log(`Square with id: ${event.target.id}`);
+      })
     })
   }
 }
