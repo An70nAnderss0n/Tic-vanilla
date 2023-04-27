1e868f7323fb3fe9767bb9520fff47ded96c8b2e
An70nAnderss0n <113804609+An70nAnderss0n@users.noreply.github.com>
2023-04-27 10:51:15 +0200

    Handles dd-function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-script.js
+"use strict";
+
+const App = {
+  // All of our selected HTML elements
+  $: {
+    menu: document.querySelector('.menu'),
+    menuItems: document.querySelector('.items'),
+  },
+
+  init() {
+    App.$.menu.addEventListener('click', (event) => {
+      App.$.menuItems.classList.toggle('hidden');
+    })
+  }
+}
+
+window.addEventListener('load', App.init)
