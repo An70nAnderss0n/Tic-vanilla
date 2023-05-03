6467c7b66ae6b94c1545d62ef0b9642783da03e6
An70nAnderss0n <113804609+An70nAnderss0n@users.noreply.github.com>
2023-05-03 12:38:56 +0200

    Adds state logic

--- store.js
+++ store.js
@@ -1,18 +1,38 @@
 const initialValue = {
   moves: [],
-}
+};
 
 
 export default class Store {
 
   #state = initialValue;
 
-  constructor() {
-
+  constructor(players) {
+    this.players = players;
   }
 
   get game() {
-    return 'Dummy value'
+    const state = this.#getState();
+
+    const currentPlayer = this.players[state.moves.length % 2];
+
+    return {
+      moves: state.moves,
+      currentPlayer,
+    };
+  }
+
+  playerMove(squareId) {
+    const state = this.#getState();
+
+    const stateClone = structuredClone(state);
+
+    stateClone.moves.push({
+      squareId,
+      player: this.game.currentPlayer,
+    })
+
+    this.#saveState(stateClone)
   }
 
   #getState() {
@@ -29,7 +49,7 @@ export default class Store {
         newState = stateOrFn(prevState)
         break;
       case 'object':
-        newState = prevState;
+        newState = stateOrFn;
         break;
       default:
         throw new Error('Invalid argument passed to saveState')
