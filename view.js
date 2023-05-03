4aaa406abd54a5ae13704cff05a54862fc8a243b
An70nAnderss0n <113804609+An70nAnderss0n@users.noreply.github.com>
2023-05-03 10:14:26 +0200

    Adds logic of player turn

--- view.js
+++ view.js
@@ -1,26 +1,37 @@
 export default class View {
 
   $ = {
+  }
+  $$ = {
 
   }
 
+
+
   constructor() {
-    this.$.menu = document.querySelector('[data-id="menu"]');
-    this.$.menuBtn = document.querySelector('[data-id="menu-btn"]');
-    this.$.menuItems = document.querySelector('[data-id="menu-items"]');
-    this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
-    this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
-    this.$.squares = document.querySelectorAll('[data-id="square"]');
-    this.$.modal = document.querySelector('[data-id="modal"]');
-    this.$.modalText = document.querySelector('[data-id="modal-text"]');
-    this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
-    this.$.turn = document.querySelector('[data-id="turn"]');
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
 
     // UI-only event listeners
     this.$.menuBtn.addEventListener('click', event => {
-      this.$.menuItems.classList.toggle('hidden')
-    })
+      this.#toggleMenu();
+    });
   }
+
+
+  /* 
+  *Register all the event listeners
+  */
 
   bindGameResetEvent(handler) {
     this.$.resetBtn.addEventListener('click', handler)
@@ -31,9 +42,70 @@ export default class View {
   }
 
   bindPlayerMoveEvent(handler) {
-    this.$.squares.forEach(square => {
+    this.$$.squares.forEach(square => {
       square.addEventListener('click', handler)
     });
+  }
+
+  /* 
+  * DOM helper methods
+  */
+
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle('hidden');
+    this.$.menuBtn.classList.toggle('border')
+
+    const icon = this.$.menuBtn.querySelector('i')
+
+    icon.classList.toggle('fa-chevron-down')
+    icon.classList.toggle('fa-chevron-up')
+  }
+
+  handlePlayerMove(squareEl, player) {
+    const icon = document.createElement('i');
+    icon.classList.add(
+      'fa-solid', player === 1
+      ? 'fa-x'
+      : 'fa-o');
+    icon.classList.add(
+      'fa-solid', player === 1
+      ? 'x-color'
+      : 'o-color');
+    squareEl.replaceChildren(icon)
+  }
+
+  // player = 1 | 2
+  setTurnIndicator(player, opponent) {
+    const icon = document.createElement('i');
+    const label = document.createElement('p');
+
+    this.$.turn.classList.add(player.colorClass);
+    this.$.turn.classList.remove(opponent.colorClass);
+
+    icon.classList.add('fa-solid', player === 1 ? 'fa-x' : 'fa-o');
+
+    label.innerText = player === 1 ? `Player 1 you're up!` : `Player 2 you're up!`;
+
+    this.$.turn.replaceChildren(icon, label)
+  }
+
+  #qs(selector, parent) {
+    const el = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector)
+
+    if (!el) throw new Error('Could not find new element')
+
+    return el;
+  }
+
+
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector)
+
+    if (!elList) throw new Error('Could not find new element')
+
+    return elList;
   }
 
 }
